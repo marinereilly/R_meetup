bca6d53c1b72120232f6ffc836f1461fed83c9de
Erin Reilly <erinmariereilly@gmail.com>
2018-05-29 16:13:17 -0400

    Initial setup for ggplot demonstration

--- cove_point_ggplot.R
+++ cove_point_ggplot.R
@@ -14,11 +14,18 @@ library(lubridate)
 library(viridis)
 
 #####Load Data#####
+salinity<-read.csv(file = "data/salinity.csv")
 
+wind1<-read.csv(file = "data/wind_freq_strength_2017.csv")
+wind2<-read.csv(file = "data/wind_freq_seasonal.csv")
 
 #####Format Data#####
+salinity$date_time<-mdy_hm(salinity$date_time)
+salinity$fake_date<-mdy_hm(salinity$fake_date)
+salinity$Year<-as.factor(salinity$Year)
 
-
+wind1$year<-as.factor(wind1$year)
+wind2$year<-as.factor(wind2$year)
 
 #####Some things to specify that I will call upon later#####
 pal7<-c("North"="#E69F00", "Mid"="#F0E442", "South"="#009E73", "Hobo"="#56B4E9", "Kayak"="#56B4E9")
@@ -29,3 +36,8 @@ compass<-c("N", "NE", "E", "SE", "S", "SW", "W", "NW")
 
 
 #####Graphing!#####
+
+
+
+
+
